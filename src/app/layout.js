3931e5011d6265ebc9a77bff19bf3c33c74176a7
Bayzid004 <bayzid4440@gmail.com>
2025-01-17 20:12:08 +0600

    2nd Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,8 @@
 import localFont from "next/font/local";
-import "./globals.css";
 import Navbar from "@/Components/Navbar";
 import NavbarContent from "@/Components/NavbarContent";
-import BannerContent from "@/Components/BannerContent";
-import PoolInstall from "@/Components/PoolInstall";
-import DefinitionPool from "@/Components/DefinitionPool";
-import DesignOptions from "@/Components/DesignOptions";
-import EasyPools from "@/Components/EasyPools";
-import EnsureLongevity from "@/Components/EnsureLongevity";
-import GalleryPool from "@/Components/GalleryPool";
-import ProcessPool from "@/Components/ProcessPool";
-import QualityExcellence from "@/Components/QualityExcellence";
-import CompareOther from "@/Components/CompareOther";
-import Expect from "@/Components/Expect";
-import BenefitsPool from "@/Components/BenefitsPool";
-import PoolContractor from "@/Components/PoolContractor";
-import BestCompany from "@/Components/BestCompany";
-import StartProject from "@/Components/StartProject";
-import ServingPool from "@/Components/ServingPool";
 import FooterPool from "@/Components/FooterPool";
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -40,28 +24,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
         <NavbarContent/>
-        <BannerContent/>
-        <PoolInstall/>
-        <DefinitionPool/>
-        <DesignOptions/>
-        <EasyPools/>
-        <EnsureLongevity/>
-        <GalleryPool/>
-        <ProcessPool/>
-        <QualityExcellence/>
-        <CompareOther/>
-        <Expect/>
-        <BenefitsPool/>
-        <PoolContractor/>
-        <BestCompany/>
-        <StartProject/>
-        <ServingPool/>
-        <FooterPool/>
+        
         {children}
+        <FooterPool/>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,39 @@
-import Image from "next/image";
+import BannerContent from "@/Components/BannerContent";
+import PoolInstall from "@/Components/PoolInstall";
+import DefinitionPool from "@/Components/DefinitionPool";
+import DesignOptions from "@/Components/DesignOptions";
+import EasyPools from "@/Components/EasyPools";
+import EnsureLongevity from "@/Components/EnsureLongevity";
+import GalleryPool from "@/Components/GalleryPool";
+import ProcessPool from "@/Components/ProcessPool";
+import QualityExcellence from "@/Components/QualityExcellence";
+import CompareOther from "@/Components/CompareOther";
+import Expect from "@/Components/Expect";
+import BenefitsPool from "@/Components/BenefitsPool";
+import PoolContractor from "@/Components/PoolContractor";
+import BestCompany from "@/Components/BestCompany";
+import StartProject from "@/Components/StartProject";
+import ServingPool from "@/Components/ServingPool";
 
-export default function Home() {
+export default function Home () {
   return (
     <main>
-      
+      <BannerContent />
+      <PoolInstall />
+      <DefinitionPool />
+      <DesignOptions />
+      <EasyPools />
+      <EnsureLongevity />
+      <GalleryPool />
+      <ProcessPool />
+      <QualityExcellence />
+      <CompareOther />
+      <Expect />
+      <BenefitsPool />
+      <PoolContractor />
+      <BestCompany />
+      <StartProject />
+      <ServingPool />
     </main>
-  );
+  )
 }
